4ad421805e431014e9e24d882084104f059f0c50
Erik Westlund <erik.westlund@student.swic.edu>
2024-11-25 20:51:32 -0600

    Add class notes

--- src/main.js
+++ src/main.js
@@ -1,31 +1,23 @@
-import {
-  countDisplayRef,
-  decrementRef,
-  incrementRef,
-  resetRef,
-} from "./lib.js";
+import Button from "./components/button";
+import Count from "./components/count";
+import state from "./state";
 
-let count = 0;
-
-function setCount(newCount) {
-  count = newCount;
-  render();
-}
+const app = document.querySelector("#app");
 
 function render() {
-  countDisplayRef.textContent = count;
+  app.innerHTML = `  ${Count(state.getState().count)}
+    <div class="flex justify-center">
+      ${Button("+1", "increment")}
+      ${Button("-1", "decrement")}
+    </div>`;
 }
 
-incrementRef.addEventListener("click", () => {
-  setCount(count + 1);
-});
-
-decrementRef.addEventListener("click", () => {
-  setCount(count - 1);
-});
-
-resetRef.addEventListener("click", () => {
-  setCount(0);
-});
-
+state.subscribe(render);
 render();
+
+document.addEventListener("click", (event) => {
+  if (event.target.id === "increment")
+    state.setState({ count: state.getState().count + 1 });
+  else if (event.target.id === "decrement")
+    state.setState({ count: state.getState().count - 1 });
+});
