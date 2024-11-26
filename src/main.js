1b54a16a219ee8729a6dd4cf666293e8b70a6fc6
Erik Westlund <erik.westlund@student.swic.edu>
2024-11-25 19:50:23 -0600

    Add counter code in main

--- src/main.js
+++ src/main.js
@@ -1,1 +1,25 @@
-// TODO: Do what must be done
+// counter.js
+let count = 0;
+
+function updateDisplay() {
+  document.getElementById("count-display").textContent = count;
+}
+
+function setCount(newCount) {
+  count = newCount;
+  updateDisplay();
+}
+
+document.getElementById("increment").addEventListener("click", () => {
+  setCount(count + 1);
+});
+
+document.getElementById("decrement").addEventListener("click", () => {
+  setCount(count - 1);
+});
+
+document.getElementById("reset").addEventListener("click", () => {
+  setCount(0);
+});
+
+updateDisplay(); // Show initial count
